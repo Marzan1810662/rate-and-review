4734fb94c9a28fd3c1af13fff9477b99c87284bc
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-04 23:51:39 +0600

    start icon added in review

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,3 +1,5 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Review.css'
 
@@ -6,10 +8,12 @@ const Review = ({ review }) => {
     return (
         <div className='review'>
             <img className='img-fluid p-4' src={picture} alt="" />
-            <div className='px-2'>
+            <div className='px-2 pb-4'>
                 <h4>{name}</h4>
                 <p><b>Review:</b> {review_text} </p>
-                <h6>Rating: {rating}</h6>
+                <div className='ratings'>
+                    <h6>Rating: {rating} <FontAwesomeIcon className='star-icon' icon={faStar}/></h6>
+                </div>
             </div>
         </div>
     );
