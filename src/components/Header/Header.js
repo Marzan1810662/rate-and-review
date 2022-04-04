e1a6cedc6496838ecd339ef2448ff9c0a2522a27
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-04 12:01:51 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" variant="light" className='mb-4'>
+                <Container>
+                    <Navbar.Brand className='fw-bolder fs-2 text-secondary' as={NavLink} to="/">Rate & Review</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link className='fs-5 fw-bold ms-5' as={NavLink} to="/home">Home</Nav.Link>
+                            <Nav.Link className='fs-5 fw-bold ms-5' as={NavLink} to="/reviews">Reviews</Nav.Link>
+                            <Nav.Link className='fs-5 fw-bold ms-5' as={NavLink} to="/dashboard">Dashboard</Nav.Link>
+                            <Nav.Link className='fs-5 fw-bold ms-5' as={NavLink} to="/blogs">Blogs</Nav.Link>
+                            <Nav.Link className='fs-5 fw-bold ms-5' as={NavLink} to="/about">About</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
